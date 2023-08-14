25424682326f3a19f62d026641aa6b4533cadd2e
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-08-14 10:49:06 -0300

    cadastro

---
+++ listar-usuario.php
@@ -0,0 +1,7 @@
+<div class="container">
+    <div class="row">
+        <div class="col mt-5">
+            <h1>BOM DIA</h1>
+        </div>
+    </div>
+</div>
---
+++ salvar-usuario.php
@@ -0,0 +1,22 @@
+<?php
+switch ($_REQUEST["acao"]) {
+    case 'cadastrar':
+        $nome = $_POST["nome"];
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+        $data_nascimento = $_POST["data_nascimento"];
+
+        $sql = "INSERT INTO usuario (nome, email, senha, data_nascimento)" VALUES ()
+        break;
+
+    case 'editar':
+        # code...
+        break;
+    case 'excluir':
+        # code...
+        break;
+
+    default:
+        # code...
+        break;
+}
